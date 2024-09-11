17d3cc25d586eb5f3634b0214c042cfa79463d40
James Leo <ocampojamesleo04@gmail.com>
2024-09-11 12:50:48 +0800

    added prisma generate to build

--- src/app/components/client/skeletonCard.tsx
+++ src/app/components/client/skeletonCard.tsx
@@ -1,105 +1,111 @@
 export default function SkeletonCard() {
   return (
     <>
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
+        <div className="skeleton h-52 w-full"></div>
+        <div className="skeleton h-4 w-28"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+      </div>
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
       </div>
 
-      <div className="flex w-56 flex-col gap-4">
+      <div className="flex md:w-52 lg:w-56 flex-col gap-4">
         <div className="skeleton h-52 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>

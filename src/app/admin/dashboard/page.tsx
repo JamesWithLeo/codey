56d14a385d2af9de53b032b26c4103b3cee9d03c
James Leo <ocampojamesleo04@gmail.com>
2024-09-20 06:30:10 +0800

    optimize object v1.1

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,15 +1,12 @@
 const prisma = new PrismaClient();
 import { PrismaClient } from "@prisma/client";
-import AddUser from "../../components/client/addUser";
 
 export default async function Dashboard() {
   const users = await prisma.users.findMany();
 
   return (
     <main className="w-full h-full p-4">
-      <div className="flex flex-col w-max gap-2">
-        <AddUser />
-      </div>
+      <div className="flex flex-col w-max gap-2"></div>
       {users.map((value) => (
         <div className="bg-gray-100 px-2 py-1" key={value.id}>
           <h1>{value.email}</h1>
